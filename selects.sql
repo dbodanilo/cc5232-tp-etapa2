36e9dc9db466a7c01340e08f559110aeda01fc1b
Danilo Bizarria <danilobizarria@gmail.com>
2020-11-21 18:49:13 -0300

    add and update auxiliary commands
    SELECT and DROP statements

--- selects.sql
+++ selects.sql
@@ -1,9 +1,23 @@
--- select * from public.passageiro
+-- SELECT *	FROM public.aeroporto;
+
+-- SELECT *	FROM public.atendente;
+
+-- SELECT *	FROM public.aviao;
+
+-- select * from public.bilhete
+
+-- SELECT *	FROM public.cidade;
+
+-- SELECT *	FROM public.comissario_voo;
+
+-- SELECT *	FROM public.compra;
+
+-- SELECT *	FROM public.funcionario;
+
+-- select * from public.passageiro;
+
+-- select * from public.piloto;
 
 -- select * from public.trajeto
 
 -- select * from public.voo
-
--- select * from public.bilhete
-
-select * from public.voo_aeroporto
